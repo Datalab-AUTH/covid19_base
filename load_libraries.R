ad82b6dc95bc04eda65538d2bc1aae62e5b1d1d9
George Vlahavas <vlahavas@gmail.com>
2020-07-23 14:09:21 +0300

    Add script that only loads libraries
    The load_libraries.R script loads all needed libraries by the dashboard
    projects. This is used so that renv doesn't get confused about which
    libaries should be installed or not.

---
+++ load_libraries.R
@@ -0,0 +1,23 @@
+#!/usr/bin/Rscript
+
+# this just lists the libraries needed by the dashboard projects, so
+# that renv can automatically pick them upl
+
+library("DT")
+library("countrycode")
+library("fs")
+library("htmltools")
+library("httr")
+library("jsonlite")
+library("leaflet")
+library("plotly")
+library("renv")
+library("reshape2")
+library("shiny")
+library("shinycssloaders")
+library("shinydashboard")
+library("shinythemes")
+library("tidyverse")
+library("wbstats")
+library("wordcloud2")
+library("zoo")
